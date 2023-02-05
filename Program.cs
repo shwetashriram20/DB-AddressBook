9cc851b70f7fd346527d3a4810b92161b469a37b
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-02-05 20:45:35 +0530

    greeting msg

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace LINQ_AddressBook
+namespace ProductReviewManagement
 {
-    public  class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to LINQ AddressBookSystem!");
+            Console.WriteLine("Welcome to AddressBook Program Using DATABASE.");
         }
     }
 }
