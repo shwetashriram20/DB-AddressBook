1b9aa4dd39e87b289e64d8d10bc66eba5314b616
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-02-05 20:52:50 +0530

    Table is Created

---
+++ Database.sql
@@ -0,0 +1,14 @@
+create database AddressBookService
+
+create database AddressBookService
+create table AddressBook
+(
+FirstName varchar(255),
+LastName varchar(255),
+Address varchar(255),
+City varchar(255),
+State varchar(255),
+zip varchar(255),
+PhoneNum varchar(255),
+Email varchar(255)
+)
